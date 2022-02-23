3801a10b4f76f91a76db8b96e01ea407ac2f0621
Daniel M. Matongo <m_matongo@icloud.com>
2022-02-23 12:37:47 +0000

    fix notes view

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -19,9 +19,48 @@ ActiveAdmin.register Note do
     f.inputs do
       f.input :subject_id, :as => :select, :collection => Subject.all.map{|s| [s.name, s.id]}
       f.input :title
-      f.input :body
-      f.input :refereces
+      f.input :body, as: :quill_editor
+      f.input :refereces, :placeholder => "add multiple references by comma"
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :body do |note|
+        div class: 'note-body' do
+          raw note.body
+        end
+      end
+      row :refereces do |note|
+        a = note.refereces.split(',')
+        a.map{|r| link_to r, r}
+
+      end
+      row :subject.name
+    end
+  end
+
+  index do
+    selectable_column
+    column :title
+    column :body do |note|
+      raw note.body.truncate(200)
+    end
+    column :refereces do |note|
+      a = note.refereces.split(',')
+      a.map{|r| link_to r, r}.join('<br><br>').html_safe
+    end
+    column :subject.name.humanize do |note|
+      link_to note.subject.name, admin_subject_path(note.subject)
+    end
+    column :created_at do |note|
+      note.created_at.strftime("%d/%m/%Y")
+    end
+    column :updated_at do |note|
+      note.updated_at.strftime("%d/%m/%Y")
+    end
+    actions
   end
 end
